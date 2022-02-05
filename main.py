4777ee5dd8ea3769b2460296d3a94913abe6235a
CDwee <96454637+CDwee@users.noreply.github.com>
2022-02-04 23:31:00 -0600

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,169 @@
+# started at 10:29 2-4-2022
+
+number_of_people = 10
+
+print(number_of_people)
+
+book_price = 10.98
+
+has_paid = False
+
+say_bye = 'Have a good day!'
+
+print(book_price, has_paid, say_bye)
+
+if True:
+    print('Hello World!')
+
+if 2 > 1:
+    print('2 is greater than 1')
+
+if 5 > 10:
+    print("This will not print because 5 is not greater than 10")
+
+raining = True
+
+if raining:
+    print('Wear a coat!')
+else:
+    print('No need for a coat!')
+
+temperature = 75
+
+if temperature > 100:
+    print("It's really hot today")
+elif temperature > 80:
+    print("It's hot today")
+elif temperature > 60:
+    print("It's warm today")
+else:
+    print("It's cold today")
+
+# while 10 > 7:
+    # print("This will print out infinitely")
+
+for i in range(4):
+    print(i)
+
+# name = input('What is your name?')
+
+# print("Hello " + name)
+
+# user_number = input("What is your age?")
+
+# print(type(user_number))
+
+# user_number = bool(input("Are you over 18?"))
+
+# print(type(user_number))
+
+# var1 = 8
+# var2 = 5
+
+# print(var2)
+# print(var1 - var2)
+# print(var1 * var2)
+
+var1 = 9
+var2 = 4
+
+if var1 < var2:
+    print(var2)
+elif var1 == var2:
+    print('tied')
+else:
+    print(var1)
+
+for i in range(8):
+    print(i)
+
+# while 10 > 9:
+    # print('hi')
+
+while True != True:
+    print('Hello')
+
+while 8 < 2:
+    print("Hey")
+
+# while 13 != 13.3:
+    # print("Yo")
+
+animals = ['cow', 'pig', 'dog', 'sheep', 'cat']
+
+print(animals)
+
+print(animals[0])
+print(animals[1])
+print(animals[-1])
+print(animals[-3])
+
+empty_list = []
+
+assorted_list = [9, False, 'w', 76.33, [123, 'peanuts'], True]
+print(assorted_list[4][1])
+
+nums = [10, 20, 30, 40]
+nums[1] = 50
+nums[2] = 60
+print(nums)
+
+animals = ['chicken', 'cow', 'penguin', 'tiger', 'koala']
+
+animals[1] = animals[4]
+print(animals)
+
+animals = ['chicken', 'cow', 'penguin', 'tiger', 'koala']
+animals2 = list(animals)
+animals[1] = animals[4]
+print(animals)
+print(animals2)
+
+animals = ['chicken', 'cow', 'penguin', 'tiger', 'koala']
+for animals in animals:
+    print(animals)
+
+print('apples'[4])
+
+fruity = 'strawberry'
+
+print(fruity[5])
+
+appliance = 'lamps'
+
+for x in appliance:
+    print(x)
+
+desserts = ['cookies', 'brownies', 'pies', 'cheesecake']
+
+print(desserts[0])
+print(desserts[2])
+print(desserts[3])
+print(desserts[-1])
+
+random = ['vanilla', 12, [43.22, 'chocolate', False], [90, 'tea'], 80]
+
+print(random[1])
+print(random[2][0])
+print(random[3][1])
+print(random[-5])
+
+numbers = [78.3, 27, 41.9, 68.7, 45, 12.2]
+numbers[4] = 25
+print(numbers)
+
+fruit_list = ['watermelon', 'strawberries',
+              'grapes', 'pineapple', 'raspberries']
+print(fruit_list)
+fruit_list[0] = 'strawberries'
+fruit_list[1] = 'watermelon'
+fruit_list[2] = 'pineapple'
+fruit_list[3] = 'grapes',
+fruit_list[4] = 'cantaloupe'
+print(fruit_list)
+
+word1 = 'balloon'
+
+for x in word1:
+    print(x)
+# Ended at 11:29 2-4-2022
